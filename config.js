dd0b2c60c1edf1424a684b20cc63fc9336ca4e2b
Ademola Ajibade <ajibadeademola34@gmail.com>
2026-01-22 03:36:07 +0100

    initial commit

--- config.js
+++
@@ -1,2 +0,0 @@
-export const API_KEY="AIzaSyBPbhbVDG6sPKexIXA4S-YMum8n3OIg_Do"
-
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { API_KEY } from "./config.js";
+import { env } from "./env.js";
 
 const chatBox = document.getElementById("chat-box");
 const userInput = document.getElementById("user-input");
@@ -31,7 +31,6 @@ window.onload = () => {
   if (simpleCountdown(5) === "Countdown finished!") {
     // localStorage.removeItem("chatHistory");
     // chatBox.innerHTML = "";
-    
   }
   console.log(savedChat);
 };
@@ -54,7 +53,7 @@ function showTyping() {
 }
 
 async function getBotReply(userMessage) {
-  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
+  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${env.API_KEY}`;
 
   try {
     const response = await fetch(url, {
